682b31c58a2aee8a16d00662a6c5a1c6b92d2e97
Nico Hsieh <nico.hsieh@ff0000.com>
2018-03-06 14:32:54 -0800

    adds preact

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,38 @@
 const path = require('path')
+const webpack = require('webpack')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-
+const isProduction = (process.env.NODE_ENV === 'production')
 
 const PATHS = {
   dist: path.resolve(__dirname, 'dist'),
   source: path.resolve(__dirname, 'source')
 }
 
+
+function getPlugins() {
+  const plugins = []
+  const copyPlugin = new CopyWebpackPlugin([
+    {
+      from: path.resolve(PATHS.source, 'index.html'),
+      to: PATHS.dist,
+      flatten: true
+    },
+    {
+      from: path.resolve(PATHS.source, 'images/*'),
+      to: path.resolve(PATHS.dist, 'images'),
+      flatten: true
+    }
+  ])
+  plugins.push(copyPlugin)
+
+  if (isProduction) {
+    const uglifyPlugin = new webpack.optimize.UglifyJsPlugin({
+      test: /\.js($|\?)/i
+    })
+    plugins.push(uglifyPlugin)
+  }
+  return plugins
+}
 
 module.exports = {
   entry: path.resolve(PATHS.source, 'js/main.js'),
@@ -35,49 +61,37 @@ module.exports = {
               ]
             }
           }
-        ]},
-        {
-          test: /\.scss$/,
-          use: [{
-            loader: 'style-loader'
-          }, {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true
-            }
-          }, {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true
-            }
-          }]
-        }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader',
+          options: {
+            sourceMap: true
+          }
+        }, {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true
+          }
+        }]
+      }
     ]
   },
   resolve: {
     modules: [
-      path.resolve(__dirname, 'node_modules')
+      'node_modules'
     ]
   },
-  plugins: [
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(PATHS.source, 'index.html'),
-        to: PATHS.dist,
-        flatten: true
-      },
-      {
-        from: path.resolve(PATHS.source, 'images/*'),
-        to: path.resolve(PATHS.dist, 'images'),
-        flatten: true
-      }
-    ])
-  ],
+  plugins: getPlugins(),
   devServer: {
     contentBase: PATHS.dist,
     compress: true,
     inline: true,
     port: 8000
   },
-  devtool: 'source-map'
+  devtool: 'cheap-source-map'
 }
