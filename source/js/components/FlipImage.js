e71da90897dc8cc8d8574c83f8e19234cdcca5b7
Nico Hsieh <nico.hsieh@ff0000.com>
2018-03-20 12:46:19 -0700

    adds demo components

---
+++ source/js/components/FlipImage.js
@@ -0,0 +1,32 @@
+// Demo Component
+import { h, render, Component } from 'preact'
+
+import '../../style/FlipImage.scss'
+
+class FlipImage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      flipped: false
+    }
+  }
+
+  flip = () => {
+    const value = !this.state.flipped
+    this.setState({
+      flipped: value
+    })
+  }
+
+  render(props, states) {
+    const flipClass = states.flipped ? 'flipped' : ''
+
+    return (
+      <div class={`flip-image ${flipClass}`} onClick={this.flip}>
+        <img src={props.src} />
+      </div>
+    )
+  }
+}
+
+export default FlipImage
--- source/js/main.js
+++ source/js/main.js
@@ -1,24 +1,14 @@
 import { h, render, Component } from 'preact'
-import Border from './components/UIBorder'
+import CTA from './components/CTA'
+import FlipImage from './components/FlipImage'
 
 import '../style/main.scss'
 
-class Test extends Component {
-  render() {
-    const word = 'Test'
-    return <div onClick={() => {alert('clicked')}}>{word}</div>
-  }
-}
 
 render((
     <div id="foo">
-        <span>Hello, world!</span>
-        <Test />
-        <Border 
-          width='50px'
-          height='130px'
-          size='3px'
-          color='purple'
-        />
+        <h1>Preact Splash Page Boilerplate</h1>
+        <FlipImage src='assets/images/duck.jpg' />
+        <CTA word='Click Me!' />
     </div>
 ), document.body)
